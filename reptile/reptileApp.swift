241d2842b606e5acb2974ceb55900519fc0c81b5
littletechturtle <little.turtle2983@gmail.com>
2025-11-24 21:26:24 +0000

    Initial Commit

---
+++ reptile/reptileApp.swift
@@ -0,0 +1,17 @@
+//
+//  reptileApp.swift
+//  reptile
+//
+//  Created by TechTurtle on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct reptileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
